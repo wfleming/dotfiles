8f606a831a324d5f24d4ca71ee31e451648cb715
Will Fleming <will@mintdigital.com>
2012-02-03 17:06:09 -0500

    rename bash file, add new zsh config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias cpwd='pwd|xargs echo -n|pbcopy'
+alias apinfo='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I'
+
+alias ls='ls -FG'
+alias ll='ls -l'
+
+alias gc='git commit'
+alias go='git checkout'
+alias gpf='git push-feature'
+
+alias memcachedstatus='echo stats | nc 127.0.0.1 11211'
---
+++ zsh/prompt.zsh
@@ -0,0 +1,16 @@
+autoload colors; colors;
+setopt prompt_subst
+
+PROMPT='%{$fg[green]%}[%c] %{$reset_color%}'
+RPROMPT='%{$fg[green]%}$(git_prompt_info) $(git_prompt_status)%{$reset_color%}'
+
+ZSH_THEME_GIT_PROMPT_PREFIX=""
+ZSH_THEME_GIT_PROMPT_SUFFIX=""
+ZSH_THEME_GIT_PROMPT_DIRTY=""
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[cyan]%} ✈"
+ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[yellow]%} ✭"
+ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%} ✗"
+ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[blue]%} ➦"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[magenta]%} ✂"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[grey]%} ✱"
