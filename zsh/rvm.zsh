bfbb782acad74fd725f098968133f339e8cb3d9f
Will Fleming <will@mintdigital.com>
2012-03-20 22:47:09 -0400

    rvm setup

--- zsh/rvm.zsh
+++ zsh/rvm.zsh
@@ -1,1 +1,5 @@
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+# This *still* produces "/Users/will/.rvm/scripts/initialize:50: __rvm_cleanse_variables: function definition file not found"
+# errors for me. Boo.
 [[ -s "/Users/will/.rvm/scripts/rvm" ]] && source "/Users/will/.rvm/scripts/rvm"
