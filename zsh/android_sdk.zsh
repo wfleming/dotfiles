98ec20eb081c906a41504a2a5f033fbd5862d934
Will Fleming <will@mintdigital.com>
2012-07-18 13:14:06 -0400

    updated android sdk settings

--- zsh/android_sdk.zsh
+++ zsh/android_sdk.zsh
@@ -1,3 +1,3 @@
 # include android tools in path
-export ANDROID_SDK_ROOT=/Applications/Installed/android-sdk-mac_x86
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools
+export ANDROID_SDK_ROOT=$HOME/android-sdk
+export PATH=$PATH:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools
