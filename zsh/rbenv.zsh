4796cff7a1fdaabead0dac9cd76c8226ee366c6b
Will Fleming <will@mintdigital.com>
2012-11-03 16:40:30 -0600

    path for binstubs for bundler with rbenv

--- zsh/rbenv.zsh
+++ zsh/rbenv.zsh
@@ -1,4 +1,5 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
+export PATH="$.bin:$PATH" # a bit dangerous, maybe, but makes bundler stubs much easier to work with!
 source "$HOME/.rbenv/completions/rbenv.zsh"
 rbenv rehash 2>/dev/null
