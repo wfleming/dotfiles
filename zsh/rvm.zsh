e800d7c9d6a1113882e605f8c205cac8cf12bf31
Will Fleming <will@mintdigital.com>
2012-11-03 10:59:03 -0600

    goodbye rvm, hello rbenv

--- zsh/rvm.zsh
+++ zsh/rvm.zsh
@@ -1,5 +1,4 @@
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+PATH=$HOME/.rbenv/bin/:$PATH # Add rbenv to PATH for scripting
 
-# This *still* produces "/Users/will/.rvm/scripts/initialize:50: __rvm_cleanse_variables: function definition file not found"
-# errors for me. Boo.
-[[ -s "/Users/will/.rvm/scripts/rvm" ]] && source "/Users/will/.rvm/scripts/rvm"
+# initialize rbenv
+eval "$(rbenv init -)"
