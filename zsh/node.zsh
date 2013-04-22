813235d574da99e5fd625f4cf7603338f1ebf312
Will Fleming <will@mintdigital.com>
2013-04-22 12:25:28 -0400

    update node/npm bin path

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,4 @@
 # include jake in the PATH for node
-export PATH=$PATH:/usr/local/share/npm/bin
+export PATH=$PATH:/usr/local/node_modules/.bin
 # make sure npm-installed modules easily requirable in node
 export NODE_PATH=/usr/local/lib/node/
