3d81d462ca4bf3f9fca461f749ffb4ae6db9a4b2
Will Fleming <will@mintdigital.com>
2013-04-24 11:41:49 -0400

    change EDITOR, add CHROME_BIN

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -5,4 +5,7 @@ zle -N self-insert url-quote-magic
 ## standard stuffs
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
-export EDITOR="mate -w"
+export EDITOR="subl -w"
+
+## chrome is used for JS tests, esp. testacular with node
+export CHROME_BIN="~/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
