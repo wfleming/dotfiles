ffabb85710fd2953a0f1b93459721f1264ca1741
Will Fleming <will@mintdigital.com>
2013-11-05 15:07:18 +0000

    tweak install script

--- install.rb
+++ install.rb
@@ -4,11 +4,12 @@
 home = ENV['HOME']
 
 # symlink basic dotfiles
+SKIP_FILES = ['install.rb', 'README.md']
 Dir.chdir File.dirname(__FILE__) do
   dotfiles_dir = Dir.pwd.sub(home + '/', '')
 
   Dir['*'].each do |file|
-    next if file == 'install.rb'
+    next if SKIP_FILES.include?(file)
     target_name = file == 'bin' ? file : ".#{file}"
     target = File.join(home, target_name)
     unless File.exist? target
