1d75bc7586c328592bd367fe6c8f23c30fbec291
Will Fleming <will@mintdigital.com>
2013-11-05 15:18:40 +0000

    more git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,9 +6,10 @@ alias apinfo='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/
 alias ls='ls -FG'
 alias ll='ls -AFGl'
 
+alias gu='git up'
+alias gs='git st'
 alias gc='git commit'
 alias go='git checkout'
-alias gpf='git push-feature'
 
 alias memcachedstatus='echo stats | nc 127.0.0.1 11211'
 
@@ -26,7 +27,19 @@ alias dbtp='rake db:test:prepare'
 alias kick_spin="kicker -r rails . -b 'spin push'"
 alias beep="echo -ne '\007'"
 
+
+
 ####### FUNCTIONS ##########
+
+# Shorten git to one letter, execute status by default if no
+# subcommand is specified
+function g {
+  if [[ $# > 0 ]]; then
+    git $@
+  else
+    git status
+  fi
+}
 
 #function for staging log messages
 staginglog() {
@@ -56,13 +69,7 @@ deexample() {
  done
 }
 
-# from http://gist.github.com/634946
-# Opens `https://github.com/<repo>/blob/<branch>/public/javascripts/app.js`
-# in your browser.
-#TODO - this is broken with current github URL structure
-hubb(){
-  repo=$(git config remote.origin.url | sed "s/^git@github\.com:\(.*\)\.git$/\1/")
-  branch=$(cat .git/HEAD | sed "s/.*\/\(.*\)$/\1/")
-  kind=$([[ $1 =~ \/$ ]] && echo "tree" || echo "blob")
-  open "https://github.com/$repo/$kind/$branch/$1"
+# top history
+function tophist {
+    cat ~/.bash_history | awk '{CMD[$1]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
 }
