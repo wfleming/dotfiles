c0945af137cd86d889870ac58da606f89c9c17a2
Will Fleming <will@mintdigital.com>
2014-01-15 14:10:44 +0000

    dir name change

--- home/zsh/rbenv.zsh
+++ home/zsh/rbenv.zsh
@@ -1,5 +1,5 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.rbenv/shims:$PATH"
-export PATH="./bin:./bundler_bin:$PATH" # a bit dangerous, maybe, but makes bundler stubs much easier to work with
+export PATH="./bin:./bundle_bin:$PATH" # a bit dangerous, maybe, but makes bundler stubs much easier to work with
 source "$HOME/.rbenv/completions/rbenv.zsh"
 rbenv rehash 2>/dev/null
