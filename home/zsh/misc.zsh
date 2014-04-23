599d0a0d5a408086dbac794cc0992a67f7962250
Will Fleming <will@mintdigital.com>
2014-04-23 15:09:26 +0100

    good old vim as default editor

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -5,7 +5,7 @@ zle -N self-insert url-quote-magic
 ## standard stuffs
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
-export EDITOR="subl -w"
+export EDITOR="vim"
 
 ## PhantomJS needs chrome path sometimes
 export CHROME_BIN="$HOME/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
