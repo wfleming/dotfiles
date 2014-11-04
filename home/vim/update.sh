683c30c6f12d23a8656fad1a0574efc6adccc879
Will Fleming <will@mintdigital.com>
2014-11-04 14:00:50 -0500

    setup.sh script for maintaing plugins

---
+++ home/vim/update.sh
@@ -0,0 +1,145 @@
+#!/bin/bash -e
+#
+# Updates Vim plugins.
+# Adapted from https://github.com/statico/dotfiles/blob/master/.vim/update.sh
+#
+# Update everything (long):
+#
+#   ./update.sh
+#
+# Update just the things from Git:
+#
+#   ./update.sh repos
+#
+# Update just one plugin from the list of Git repos:
+#
+#   ./update.sh repos powerline
+#
+
+cd ~
+
+vimdir=$PWD/.vim
+bundledir=$vimdir/bundle
+tmp=/tmp/$LOGNAME-vim-update
+me=.vim/update.sh
+
+# URLS --------------------------------------------------------------------
+
+# This is a list of all plugins which are available via Git repos. git:// URLs
+# don't work.
+repos=(
+
+  https://github.com/Lokaltog/vim-powerline.git
+  https://github.com/altercation/vim-colors-solarized.git
+  https://github.com/ap/vim-css-color.git
+  https://github.com/docunext/closetag.vim.git
+  https://github.com/elzr/vim-json.git
+  https://github.com/junegunn/goyo.vim.git
+  https://github.com/kchmck/vim-coffee-script.git
+  https://github.com/kien/ctrlp.vim.git
+  https://github.com/pangloss/vim-javascript.git
+  https://github.com/scrooloose/nerdtree.git
+  https://github.com/tpope/vim-fugitive.git
+  https://github.com/tpope/vim-markdown.git
+  https://github.com/vim-ruby/vim-ruby.git
+
+  )
+
+# Here's a list of everything else to download in the format
+# <destination>;<url>[;<filename>]
+other=(
+
+  )
+
+mode=$1
+if [ -z "$mode" ]; then
+  mode="all"
+fi
+
+case "$mode" in
+
+  # GIT -----------------------------------------------------------------
+  repos|repo)
+    mkdir -p $bundledir
+    for url in ${repos[@]}; do
+      if [ -n "$2" ]; then
+        if ! (echo "$url" | grep "$2" &>/dev/null) ; then
+          continue
+        fi
+      fi
+      dest="$bundledir/$(basename $url | sed -e 's/\.git$//')"
+      rm -rf $dest
+      echo "Cloning $url into $dest"
+      git clone -q $url $dest
+      rm -rf $dest/.git
+    done
+    ;;
+
+  # TARBALLS AND SINGLE FILES -------------------------------------------
+  other)
+    set -x
+    mkdir -p $bundledir
+    rm -rf $tmp
+    mkdir $tmp
+    pushd $tmp
+
+    for pair in ${other[@]}; do
+      parts=($(echo $pair | tr ';' '\n'))
+      name=${parts[0]}
+      url=${parts[1]}
+      filename=${parts[2]}
+      dest=$bundledir/$name
+
+      rm -rf $dest
+
+      if echo $url | egrep '.zip$'; then
+        # Zip archives from VCS tend to have an annoying outer wrapper
+        # directory, so unpacking them into their own directory first makes it
+        # easy to remove the wrapper.
+        f=download.zip
+        $curl -L $url >$f
+        unzip $f -d $name
+        mkdir -p $dest
+        mv $name/*/* $dest
+        rm -rf $name $f
+
+      else
+        # Assume single files. Create the destination directory and download
+        # the file there.
+        mkdir -p $dest
+        pushd $dest
+        if [ -n "$filename" ]; then
+          $curl -L $url >$filename
+        else
+          $curl -OL $url
+        fi
+        popd
+
+      fi
+
+    done
+
+    popd
+    rm -rf $tmp
+    ;;
+
+  # HELP ----------------------------------------------------------------
+
+  all)
+    $me repos
+    $me other
+    echo
+    echo "Update OK"
+    ;;
+
+  *)
+    set +x
+    echo
+    echo "Usage: $0 <section> [<filter>]"
+    echo "...where section is one of:"
+    grep -E '\w\)$' $me | sed -e 's/)//'
+    echo
+    echo "<filter> can be used with the 'repos' section."
+    exit 1
+
+esac
