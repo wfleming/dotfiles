c6fcd4a882e9ea843b1d084ac4f5c7ed451e7800
Will Fleming <will@mintdigital.com>
2014-11-21 17:06:00 -0500

    i've given up keeping Chrome in ~/Applications
    it makes too many other things difficult, and makes using 1pass
    basically impossible.

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -8,8 +8,5 @@ export LC_CTYPE=en_US.UTF-8
 export EDITOR="vim"
 export BUNDLER_EDITOR="subl"
 
-## PhantomJS needs chrome path sometimes
-export CHROME_BIN="$HOME/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
 # Docker Daemon needs this (http://docs.docker.io/en/latest/installation/mac/)
 export DOCKER_HOST=tcp://
