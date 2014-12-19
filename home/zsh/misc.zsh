e8e353e25fed6f07d0e44fcad16947a8dbb4fbe8
Will Fleming <will@mintdigital.com>
2014-12-19 15:51:03 -0500

    env update for bbox & nokogiri install

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -13,3 +13,9 @@ export PATH="$PATH:$GOPATH/bin"
 
 # Docker Daemon needs this (http://docs.docker.io/en/latest/installation/mac/)
 export DOCKER_HOST=tcp://
+
+# nokogiri doesn't like building from homebrew libxml2
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
+
+# for Mint BloomBox
+export PATH="$PATH:/usr/local/bloombox/bin"
