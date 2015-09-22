ca4d4810ce840e51088c07f9f87342890078884b
Will Fleming <will@flemi.ng>
2015-09-21 22:20:08 -0400

    put docker stuff in a file, rm old env var

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -7,18 +7,3 @@ export PAGER=less
 export LC_CTYPE=en_US.UTF-8
 export EDITOR="vim"
 export BUNDLER_EDITOR="subl"
-
-# Add Go to path
-export GOPATH="$HOME/projects/go"
-export PATH="$PATH:$GOPATH/bin"
-export GO15VENDOREXPERIMENT=1 
-
-# nokogiri doesn't like building from homebrew libxml2
-export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
-
-# for boot2docker/docker machine
-if command -v boot2docker > /dev/null 2>&1; then
-  eval "$(boot2docker shellinit 2> /dev/null)"
-else
-  eval "$(docker-machine env default)"
-fi
