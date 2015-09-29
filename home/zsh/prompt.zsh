9e75506076061d957d17cc1e7874bf363671d361
Will Fleming <will@flemi.ng>
2015-09-29 14:04:42 -0400

    git prompt emoji

--- home/zsh/prompt.zsh
+++ home/zsh/prompt.zsh
@@ -1,18 +1,18 @@
 autoload colors; colors;
 setopt prompt_subst
 
-PROMPT='%{$fg[green]%}[%c] %{$reset_color%}'
+PROMPT='%{$fg[green]%}[%c] %{$reset_color%} '
 RPROMPT='%{$fg[green]%}$(git_prompt_info) $(git_prompt_status)%{$reset_color%}'
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_DIRTY=""
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[cyan]%} ✈"
+ZSH_THEME_GIT_PROMPT_DIRTY="💩"
+ZSH_THEME_GIT_PROMPT_CLEAN="✨"
+ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[cyan]%} ✈️"
 ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[yellow]%} ✭"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%} ✗"
+ZSH_THEME_GIT_PROMPT_DELETED="❌"
 ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[blue]%} ➦"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[magenta]%} ✂"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[magenta]%} ✂️"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[grey]%} ✱"
 
 
