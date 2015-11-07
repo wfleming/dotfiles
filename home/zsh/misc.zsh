5208cf116c9b9ab4080e7bb27771adf110bb9afd
Will Fleming <will@flemi.ng>
2015-11-07 00:32:22 -0500

    zsh: set COLORTERM, make EDITOR=nvim

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -3,7 +3,8 @@ autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
 ## standard stuffs
+export COLORTERM=xterm-256color
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
-export EDITOR="vim"
+export EDITOR="nvim"
 export BUNDLER_EDITOR="subl"
