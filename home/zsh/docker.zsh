90d04c59a612d4a9103aecf50629cf91f41db84e
Will Fleming <will@flemi.ng>
2015-11-07 20:53:34 -0500

    docker utils: consistent naming, make env easily callable

--- home/zsh/docker.zsh
+++ home/zsh/docker.zsh
@@ -1,11 +1,14 @@
 # for boot2docker/docker machine
-if command -v boot2docker > /dev/null 2>&1; then
-  eval "$(boot2docker shellinit 2> /dev/null)"
-elif command -v docker-machine > /dev/null 2>&1; then
-  eval "$(docker-machine env default)"
-fi
+function docker-env {
+  if command -v boot2docker > /dev/null 2>&1; then
+    eval "$(boot2docker shellinit 2> /dev/null)"
+  elif command -v docker-machine > /dev/null 2>&1; then
+    eval "$(docker-machine env default)"
+  fi
+}
+docker-env
 
-function docker_cleanup {
+function docker-cleanup {
   docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs docker rm
   docker images | fgrep "<none>" | awk '{print $3}' | xargs docker rmi
 }
