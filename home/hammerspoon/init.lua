9cc14683b3ba7f315f5f00b88b849df884e6cb23
Will Fleming <will@flemi.ng>
2016-01-12 23:38:29 -0500

    hammerspoon

---
+++ home/hammerspoon/init.lua
@@ -0,0 +1,16 @@
+local hotkey = require "hs.hotkey"
+
+-- window tiling management
+local tiling = require "hs.tiling"
+local tilemash = {"ctrl", "cmd"}
+
+hotkey.bind(tilemash, "c", function() tiling.cycleLayout() end)
+hotkey.bind(tilemash, "j", function() tiling.cycle(1) end)
+hotkey.bind(tilemash, "k", function() tiling.cycle(-1) end)
+hotkey.bind(tilemash, "space", function() tiling.promote() end)
+hotkey.bind(tilemash, "f", function() tiling.goToLayout("fullscreen") end)
+
+-- If you want to set the layouts that are enabled
+tiling.set('layouts', {
+  'fullscreen', 'main-vertical', 'ga-vertical'
+})
