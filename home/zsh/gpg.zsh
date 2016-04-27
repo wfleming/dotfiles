eec5e57cef091832b19548d038a8c4f8653e4dc3
Will Fleming <will@flemi.ng>
2016-04-27 11:47:11 -0400

    tweak gpg-agent setup

--- home/zsh/gpg.zsh
+++ home/zsh/gpg.zsh
@@ -1,10 +1,10 @@
 if command -v gpg-agent > /dev/null 2>&1; then
   GPG_TTY=$(tty)
   export GPG_TTY
-  if [ -f "${HOME}/.gnupg/gpg-agent-info" ]; then
-    source "${HOME}/.gnupg/gpg-agent-info"
+  if [ -f "/tmp/gpg-agent-info" ]; then
+    source "/tmp/gpg-agent-info"
     export GPG_AGENT_INFO
   else
-    eval "$(gpg-agent --daemon --write-env-file "${HOME}/.gnupg/gpg-agent-info")"
+    eval "$(gpg-agent --daemon)"
   fi
 fi
