c9b2d6143c4b6f0d1bff6bcc1418c7b2ae3230d4
Will Fleming <will@flemi.ng>
2016-08-10 12:46:07 -0400

    don't need these settings on linux

--- home/zsh/misc.zsh
+++ home/zsh/misc.zsh
@@ -3,8 +3,6 @@ autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
 ## standard stuffs
-export COLORTERM=xterm-256color
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
 export EDITOR="nvim"
-export BUNDLER_EDITOR="subl"
