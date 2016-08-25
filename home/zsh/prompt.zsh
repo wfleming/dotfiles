31e09c4d21ad9afb76024008e88d129c939cd915
Will Fleming <will@flemi.ng>
2016-08-24 20:57:48 -0400

    remove git fanciness
    I don't really pay attention to this stuff. branch name is enough.

--- home/zsh/prompt.zsh
+++ home/zsh/prompt.zsh
@@ -2,19 +2,11 @@ autoload colors; colors;
 setopt prompt_subst
 
 PROMPT='%{$fg[green]%}[%c] %{$reset_color%} '
-GIT_PROMPT='%{$fg[green]%}$(git_prompt_info) $(git_prompt_status)%{$reset_color%}'
+GIT_PROMPT='%{$fg[green]%}$(git_prompt_info)%{$reset_color%}'
 RPS1="$GIT_PROMPT"
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[brown]%}☃"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}✦"
-ZSH_THEME_GIT_PROMPT_ADDED="⨁"
-ZSH_THEME_GIT_PROMPT_MODIFIED="%{$fg[yellow]%}✭"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%}⨂"
-ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[blue]%}➔"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[magenta]%}☈"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[grey]%}✱"
 
 # show VI insert mode
 function zle-line-init zle-keymap-select {
