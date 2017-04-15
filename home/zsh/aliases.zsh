e491649b36a659fe339ccb02122e31f68bcb1c55
Will Fleming <will@flemi.ng>
2017-04-15 10:56:06 -0400

    zsh cleanup
    - I don't use node natively anymore if I can possibly avoid it, certainly not globally.
    - Rarely need to run ruby tools like this directly either
    - Reformat an alias for readability

--- home/zsh/aliases.zsh
+++ home/zsh/aliases.zsh
@@ -3,9 +3,6 @@
 alias ls='ls -F --color=auto'
 alias ll='ls -AFhl'
 
-alias rc='bin/rails c'
-alias bi='bundle install'
-alias be='bundle exec'
 alias cca='codeclimate analyze'
 alias pacman-rm-orphans='sudo pacman -Rsn $(pacman -Qqdt)'
 
@@ -39,5 +36,9 @@ beep() {
 
 # top history
 tophist() {
-  cat ~/.zsh_history|cut -d ';' -f 2- 2>/dev/null| awk '{a[$1]++ } END{for(i in a){print a[i] " " i}}'|sort -rn|head
+  cat ~/.zsh_history \
+    | cut -d ';' -f 2- 2>/dev/null \
+    | awk '{a[$1]++ } END{for(i in a){print a[i] " " i}}' \
+    | sort -rn \
+    | head
 }
--- home/zsh/node.zsh
+++
@@ -1,4 +0,0 @@
-# include jake in the PATH for node
-export PATH=$PATH:./node_modules/.bin:/usr/local/share/npm/bin
-# make sure npm-installed modules easily requirable in node
-export NODE_PATH=/usr/local/lib/node_modules/
