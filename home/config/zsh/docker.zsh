aeabec1b65fca9d94f25b19e8d2cc47868338806
Will Fleming <will@flemi.ng>
2017-08-30 22:16:04 -0400

    Improve docker-cleanup
    - Look for older containers (I don't run this often enough)
    - Do not try to remove running containers (I have some long running
      containers)
    - Don't run `xargs` with empty input, it's noisy

--- home/config/zsh/docker.zsh
+++ home/config/zsh/docker.zsh
@@ -1,4 +1,6 @@
 function docker-cleanup {
-  docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs docker rm
-  docker images | fgrep "<none>" | awk '{print $3}' | xargs docker rmi
+  docker ps -a | grep --extended-regexp " (weeks|months) ago " | \
+    grep -v " Up " | awk '{print $1}' | xargs --no-run-if-empty docker rm
+  docker images | fgrep "<none>" | awk '{print $3}' | \
+    xargs --no-run-if-empty docker rmi
 }
