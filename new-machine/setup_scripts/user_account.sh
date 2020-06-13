7705e5a3a4eccb13e403a6dd2640062db38ca637
Will Fleming <will@flemi.ng>
2020-06-13 14:25:41 -0400

    new-machine user account: docker group
    Did this manually this time, tracking for the future.

--- new-machine/setup_scripts/user_account.sh
+++ new-machine/setup_scripts/user_account.sh
@@ -5,7 +5,12 @@ setup_base_dir=$(dirname $0)
 . "${setup_base_dir}/shared.sh"
 
 echo "== Creating user account"
-useradd "${username}" --create-home --home-dir /home/will --shell /usr/bin/zsh --user-group -G wheel
+useradd "${username}" \
+  --create-home \
+  --home-dir /home/will \
+  --shell /usr/bin/zsh \
+  --user-group \
+  --groups wheel,docker
 
 echo "==== set a password for ${username}"
 passwd "${username}"
