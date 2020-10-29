fccc6c845855f0a6eb31545f0ca263f73e582803
Will Fleming <will@flemi.ng>
2020-10-29 09:34:59 -0400

    system files install script - copy some files instead of symlinking
    Symlinks are fine for systemd unit files, but it would seem that it
    ignores some config files like logind.conf.d/* if they're symlinks, so
    my configuration for logind was being ignored. I've specialized my
    install script to actually copy this file rather than symlink it.
    
    I suppose there might be security concerns with following symlinks here,
    but if there are, wouldn't they equally apply to unit files?

--- system/install.sh
+++ system/install.sh
@@ -68,6 +68,30 @@ link() {
   fi
 }
 
+# ARGS: (source, dest)
+copy() {
+  if [ -e "$2" ]; then
+    if [ -L "$2" ]; then
+      warn "$2 is a symlink, but it should be a regular file"
+    elif diff --brief "$1" "$2" &> /dev/null; then
+      echo "$2 already exists with the same contents as $1"
+    else
+      warn "$2 exists but has different contents than $1"
+    fi
+  elif [ "$dry_run" = 1 ]; then
+    success "[DRY RUN] Would copy $1 to $2"
+  else
+    if [ ! -d "$(dirname "$2")" ]; then
+      mkdir -p "$(dirname "$2")"
+    fi
+    if cp "$1" "$2"; then
+      success "Copied $1 to $2"
+    else
+      warn "Failed to copy $1 to $2"
+    fi
+  fi
+}
+
 should_link() {
   source="$1"
   case "$source" in
@@ -80,6 +104,18 @@ should_link() {
   esac
 }
 
+link_type() {
+  source="$1"
+  case "$source" in
+    *systemd/logind.conf.d/*)
+      echo "copy"
+      ;;
+    *)
+      echo "soft-link"
+      ;;
+  esac
+}
+
 ############# DO THE LINKING #############
 
 # Unlike ../install.sh, do not link dirs - in /etc that's a recipe for trouble.
@@ -88,7 +124,14 @@ root_path=$(dirname $(realpath $0))
 for f in $(find "$root_path" -type f -not -name install.sh); do
   if should_link "$f"; then
     rel_target=$(realpath --relative-to="$root_path" "$f")
-    link "$(realpath "$f")" "/$rel_target"
+    case "$(link_type "$f")" in
+      copy)
+        copy "$(realpath "$f")" "/$rel_target"
+        ;;
+      *)
+        link "$(realpath "$f")" "/$rel_target"
+        ;;
+    esac
   fi
 done
 
