b93430389ca280290e5dcab5225a288e9d88930e
Will Fleming <will@flemi.ng>
2021-01-12 17:18:51 -0500

    zsh - config edit-command-line

--- home/config/zsh/misc.zsh
+++ home/config/zsh/misc.zsh
@@ -2,6 +2,11 @@
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
+## binding to edit command line in EDITOR
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
 ## standard stuffs
 export PAGER=less
 export LC_CTYPE=en_US.UTF-8
