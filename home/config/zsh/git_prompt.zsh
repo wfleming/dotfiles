6c25de325dad883c9bf2e7ff91b35b2144e3f5cb
Will Fleming <will@flemi.ng>
2021-02-03 10:55:50 -0500

    try pure zsh prompt
    you convinced me, max

--- home/config/zsh/git_prompt.zsh
+++
@@ -1,6 +0,0 @@
-# get the name of the branch we are on
-function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "${ref#refs/heads/}"
-}
-
--- home/config/zsh/prompt.zsh
+++ home/config/zsh/prompt.zsh
@@ -1,16 +1,2 @@
-autoload -U colors && colors
-setopt prompt_subst
-
-PROMPT='%{$fg[green]%}[%c] %{$reset_color%} '
-GIT_PROMPT='%{$fg[green]%}$(git_prompt_info)%{$reset_color%}'
-RPS1="$GIT_PROMPT"
-
-# show VI insert mode
-function zle-line-init zle-keymap-select {
-  VIM_MODE="%{$fg_bold[yellow]%} [% NORMAL]% %{$reset_color%}"
-  RPS1="${${KEYMAP/vicmd/$VIM_MODE}/(main|viins)/} $GIT_PROMPT $EPS1"
-  zle reset-prompt
-}
-
-zle -N zle-line-init
-zle -N zle-keymap-select
+autoload -U promptinit; promptinit
+prompt pure
