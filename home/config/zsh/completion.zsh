00c742389571f66bfb6d6dadb487059d07ca0c00
Will Fleming <will@flemi.ng>
2021-04-05 09:44:02 -0400

    add tab completion for custom git subcommands

--- home/config/zsh/completion.zsh
+++ home/config/zsh/completion.zsh
@@ -14,7 +14,7 @@ compdef g=git
 
 zmodload -i zsh/complist
 
-## case-insensitive (all),partial-word and then substring completion
+# case-insensitive (all),partial-word and then substring completion
 if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
   zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
   unset CASE_SENSITIVE
@@ -23,3 +23,14 @@ else
 fi
 
 zstyle ':completion:*' list-colors ''
+
+# tab complete some custom git sub-commands
+# I found the details about this in /usr/share/zsh/functions/Completion/Unix/_git
+# for some reason the `compdef` above doesn't apply to these, so they're defined twice
+git_user_cmds=(
+  jira-branch:'Checkout branch for a jira ticket'
+  prune-branches:'Delete branches already merged into master'
+  cleanup-branches:'Interactive mass-delete branches'
+)
+zstyle ':completion:*:*:git:*' user-commands $git_user_cmds
+zstyle ':completion:*:*:g:*' user-commands $git_user_cmds
