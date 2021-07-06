46ca33cfe6fb8197da3795dd2188087effe43325
Will Fleming <will@flemi.ng>
2021-07-06 09:41:54 -0400

    Fix wifi-nfs.sh udev script
    These kinds of little things always seems to end up more complicated
    than I'd like.
    
    I left a comment in the script mostly explaining why I ended up doing it
    this way, I'll re-explain and add more context here.
    
    There aren't any good udev events to listen to when a connection to a
    WiFi network is established. The creation of the device itself is about
    as close as I've figured out how to get, which is highly imperfect, but
    for my purposes is good enough. (It happens anytime iwd is restarted,
    and I think also happens when waking up from hibernation. Since the
    thing I care about here is "moving from home to somewhere else or
    somewhere else back home", that's good enough.)
    
    Even when that event does trigger in those cases, the connection is not
    established immediately - you need to wait a few seconds before the
    device is connected. Sleeping for long periods in udev scripts isn't
    great, since they block other udev scripts from running (I discovered
    that while working on some scripting for Kodi on my HTPC).
    
    So the workaround is to have the script, when called from udev,
    basically fork itself to the background and have the fork sleep and then
    run. This way the script can wait as long as it needs to without
    blocking udev from other work.
    
    One small note: I did try `&` instead of `nohup` here and it did not
    work. I didn't spent much time figuring out why. The
    backgrounded-function called via `&` did start, but the logic after the
    `sleep` never ran. I am not sure why this is - my understanding was `&`
    subshells normally survive their parents exiting. Maybe what's happening
    is that `&` gets re-parented to the parent of the originating process,
    and doesn't survive *that* exiting? (I.e. when udev finishes processing
    triggers.)

--- system/usr/local/bin/wifi-nfs.sh
+++ system/usr/local/bin/wifi-nfs.sh
@@ -8,9 +8,31 @@ home_network_is_connected() {
   iwctl station wlan0 show | grep --quiet "Connected network.*$network"
 }
 
-if home_network_is_connected; then
-  systemctl --no-block start mnt-mother-data.automount
+check_network_after_delay() {
+  sleep 5
+  if home_network_is_connected; then
+    systemctl --no-block start mnt-mother-data.automount
+  else
+    systemctl --no-block stop mnt-mother-data.automount
+    systemctl --no-block stop mnt-mother-data.mount
+  fi
+}
+
+# Entrypoint logic when the script runs:
+#
+# Immediately after the device is added, it's probably not already connected.
+# And AFAICT there isn't another udev event we can listen to that would trigger
+# after the network is connected, so the only practical approach I can see is to
+# wait a few seconds before checking the network. However, just `sleep`-ing in a
+# udev script is bad practice from what I've seen, since udev triggers run in
+# series, i.e. they block.
+#
+# So the solution is that when the script runs without any arguments, all it
+# does is nohup itself with a flag to tell it to "really" run the script logic.
+# When it "really" runs in a forked shell, it sleeps for a few seconds. This way
+# we can wait for a while without blocking other udev triggers.
+if [ "$1" = "-r" ]; then
+  check_network_after_delay
 else
-  systemctl --no-block stop mnt-mother-data.automount
-  systemctl --no-block stop mnt-mother-data.mount
+  nohup "$0" -r
 fi
