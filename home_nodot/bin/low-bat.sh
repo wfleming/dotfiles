5794b3fac9836b5d7f5e7731ffb2374723a55eb0
Will Fleming <will@flemi.ng>
2022-11-01 11:37:22 -0400

    Remove accidentally committed low-bat.sh
    Didn't intend to commit this. It wasn't really being used. I had tried
    to hook up some udev rules to trigger when battery was low to warn me
    but never got it working correctly.

--- home_nodot/bin/low-bat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Expected to be run as root via udev
-
-sudo -u will \
-  DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id --user will)/bus \
-  notify-send --urgency=critical --wait "Battery is low" "Charge your battery."
