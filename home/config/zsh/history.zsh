c0113a43d77a1fad58a749ba3d046615d6c6b7be
Will Fleming <will@flemi.ng>
2024-01-22 11:49:46 -0500

    annotate history options a bit

--- home/config/zsh/history.zsh
+++ home/config/zsh/history.zsh
@@ -5,8 +5,8 @@ SAVEHIST=1000000
 setopt hist_ignore_dups # ignore duplication command history list
 setopt share_history # share command history data
 
-setopt inc_append_history
-setopt extended_history
+setopt inc_append_history # append immediately
+setopt extended_history # store timestops
 setopt hist_expire_dups_first
 setopt hist_ignore_space
 
