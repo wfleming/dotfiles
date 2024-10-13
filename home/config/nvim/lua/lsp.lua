4f649aed03e015cfcaa6d272b2f63bd0ffe1a198
Will Fleming <will@flemi.ng>
2024-10-12 20:44:53 -0400

    rm old comment

--- home/config/nvim/lua/lsp.lua
+++ home/config/nvim/lua/lsp.lua
@@ -6,7 +6,6 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 lspconfig.solargraph.setup({
   on_new_config = function(new_config, new_root_dir)
     base_cmd = lspcontainers.command('solargraph', { root_dir = new_root_dir })
-    -- new_config.cmd = {table.unpack(base_cmd), '/usr/bin/solargraph', 'stdio'}
     new_config.cmd = util.list_concat(base_cmd, {'/usr/bin/solargraph', 'stdio'})
   end,
   capabilities = capabilities,
